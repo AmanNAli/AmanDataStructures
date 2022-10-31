de04044a217f3217afaa2b48019991e3291aebe7
AmanNAli <aman.ali_ug24@ashoka.edu.in>
2022-10-31 08:21:56 +0000

    Updated selection sort to sort lists

--- selectionsort.c
+++ selectionsort.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct node {
+    int val;
+    struct node * next;
+};
+typedef struct node * NodeAddress;
+NodeAddress linkedListFromArray(int * a, int n)
+{
+    NodeAddress head = NULL;
+    NodeAddress temp = NULL;
+    if(n>0)
+    {
+        head = malloc(sizeof(struct node));
+        head->val = a[0];
+        head->next = NULL;
+        temp = head;
+    }
+    for(int i=1;i<n;i++)
+    {
+    temp->next = malloc(sizeof(struct node));
+    temp = temp->next;
+    temp->val = a[i];
+    temp->next = NULL;
+    }
+    return head;
+}
 int findMaxPos(int * a,int n)
 {
     int max=0;
@@ -23,10 +48,32 @@ void SelSort(int * a,int n)
         a[x] = t;
     }
 }
+void SelSortList(NodeAddress head)
+{
+    NodeAddress temp;
+    NodeAddress min;
+    while(head)
+    {
+        temp = head;
+        min = temp;
+        while(temp)
+        {
+            if(min->val > temp->val)
+            {
+                min = temp;
+            }
+            temp = temp->next;
 
+        }
+        int small = head->val;
+        head->val = min->val;
+        min->val = small;
+        head = head->next;
+    }
+}
 int main(int argv,char **argc)
 {
-     int n;
+    int n;
     printf("Enter array length\n");
     scanf("%d",&n);
     int * a = malloc(n * sizeof(int));
@@ -35,10 +82,17 @@ int main(int argv,char **argc)
     {
         scanf("%d",&a[i]);
     }
+    NodeAddress head = linkedListFromArray(a,n);
     SelSort(a,n);
+    SelSortList(head);
     printf("The sorted array is:");
     for(int i =0;i<n;i++)
     {
         printf(" %d ",a[i]);
+    }
+    printf("\nThe sorted list is:");
+    for(NodeAddress c = head;c!=NULL;c=c->next)
+    {
+        printf(" %d ",c->val);
     }
 }
