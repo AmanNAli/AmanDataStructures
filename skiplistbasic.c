72a4951d8132c97f1ee9ec059cf213a8d74e6049
AmanNAli <113897123+AmanNAli@users.noreply.github.com>
2022-12-04 00:22:14 +0530

    Added comments, sorting for the input array

--- skiplistbasic.c
+++ skiplistbasic.c
@@ -1,36 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+// I use expressway to describe the nodes that skip because Professor's use of that term got stuck in my head
 struct node
 {
     int val;
-    int levels;
+    int levels; // tracks number of levels
     struct node **next;
 };
 typedef struct node *NodeAddress;
-NodeAddress skipify(NodeAddress head)
+
+// since my method for creating the skip list requires a sorted array, I had to include quicksort. Skip to line 52 for the actual skip list code
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int *a, int start, int stop)
+{
+    int pivot = a[start];
+    int i = start + 1;
+    int j = stop;
+    while (i <= j)
+    {
+        if (a[i] > pivot)
+        {
+            swap(&a[i], &a[j]);
+            j = j - 1;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    swap(&a[start], &a[j]);
+    return j;
+}
+void quickSort(int *a, int start, int stop)
+{
+    if (stop >= start)
+    {
+
+        int p = partition(a, start, stop);
+        quickSort(a, start, p - 1);
+        quickSort(a, p + 1, stop);
+    }
+}
+
+NodeAddress skipify(NodeAddress head) // function to connect the expressway
 {
     NodeAddress temp = head;
     while (temp)
     {
-        if (temp->levels == 2)
+        if (temp->levels == 2) // if the current node is part of the expressway, connect it to the following node in the expressway
         {
             NodeAddress c = temp->next[0];
-            while (c && c->levels < 2)
+            while (c && c->levels < 2) // finds the next node that is part of the expressway
             {
                 c = c->next[0];
             }
             temp->next[1] = c;
-            temp = temp->next[1];
+            temp = temp->next[1]; // move down the expressway
         }
         else
-            temp = temp->next[0];
+            temp = temp->next[0]; // if the current node is just a normal node, move to the next one
     }
     return head;
 }
 NodeAddress skipListfromArr(int *a, int n)
 {
     NodeAddress head = malloc(sizeof(struct node));
-    head->levels = 2;
+    head->levels = 2; // I chose to always make the head node part of the expressway
     head->next = malloc(2 * sizeof(struct node *));
     head->val = a[0];
     int skip = 1;
@@ -39,8 +80,8 @@ NodeAddress skipListfromArr(int *a, int n)
     {
         temp->next[0] = malloc(sizeof(struct node));
         temp = temp->next[0];
-        skip = rand() % 5;
-        if (skip % 5 == 0)
+        skip = rand() % 5; // gives a random number between 0 and 4
+        if (skip % 5 == 0) // 1 in 5 chance to connect the current node to the 'expressway'
         {
             temp->levels = 2;
         }
@@ -52,7 +93,7 @@ NodeAddress skipListfromArr(int *a, int n)
         temp->next = malloc(temp->levels * sizeof(struct node *));
         temp->val = a[i];
     }
-    head = skipify(head);
+    head = skipify(head); // connect the expressway
     return head;
 }
 
@@ -64,14 +105,14 @@ void search(NodeAddress temp, int x)
         printf("Not found\n");
         return;
     }
-    while (temp->next[1] && x > temp->next[1]->val)
+    while (temp->next[1] && x > temp->next[1]->val) // take the expressway as far as possible
     {
         temp = temp->next[1];
     }
-    while (temp)
+    while (temp) // now that we know x lies in a certain range (given that it is in the list at all), we must go one by one
     {
-        if (x < temp->val)
-            break;
+        if (x < temp->val) // since it is a sorted list, if x is smaller than the current value we can conclude that we won't find x further ahead in the list
+            break;         // we must have found all appearances of x already if there were any
         if (x == temp->val)
         {
             printf("Found\n");
@@ -82,10 +123,17 @@ void search(NodeAddress temp, int x)
     if (flag == 0)
         printf("Not found\n");
 }
+
 int main()
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int n = sizeof(a) / sizeof(a[0]);
+    int n = 11;
+    int *a = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 100;
+    }
+    quickSort(a, 0, n - 1);
     NodeAddress head = skipListfromArr(a, n);
     NodeAddress temp = head;
     while (temp)
@@ -99,5 +147,5 @@ int main()
         printf("%d\n", temp->val);
         temp = temp->next[1];
     }
-    search(head, -1);
+    search(head, 86);
 }
